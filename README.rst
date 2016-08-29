9a802c787547fb108a2982df9d9c872787c54d1e
ll1l11 <ll1l11@qq.com>
2016-08-30 01:14:37 +0800

    2

---
+++ README.rst
@@ -0,0 +1,3 @@
+- 开发使用 python manage.py runserver
+
+- 生产环境使用 uswgi + 环境变量的配置方式
---
+++ curl2py_online/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+
+
+def create_app(cfg=None):
+    app = Flask(__name__)
+    return app
